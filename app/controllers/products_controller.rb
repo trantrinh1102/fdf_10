08cae6f0dba4106660fc72628c6ccdd6d399277e
trantrinh1102 <trantrinh.1102@gmail.com>
2016-09-09 12:59:56 +0700

    show detail product

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,6 @@
+class ProductsController < ApplicationController
+  load_and_authorize_resource
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root "static_pages#home"
   resources :users, only: [:show]
 
+  resources :products, only: [:show]
   namespace :admin do
     root "users#index"
     resources :categories
