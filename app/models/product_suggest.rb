29668196bc86e34f8738ae5b155c51cf84c1bf74
phamvanbk11 <phamvanbk11@gmail.com>
2016-09-13 14:46:51 +0700

    76114 suggest product

--- app/models/product_suggest.rb
+++ app/models/product_suggest.rb
@@ -1,4 +1,8 @@
 class ProductSuggest < ActiveRecord::Base
+  mount_uploader :image, ImageUploader
   belongs_to :user
   belongs_to :category
+
+  validates_processing_of :image
+  validates :product_name, presence: true
 end
